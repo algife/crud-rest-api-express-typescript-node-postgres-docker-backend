9d635683724b794948f82afb2adb697f27a4def3
Alexandre Gimenez <algife@gmail.com>
2022-04-12 12:45:33 +0300

    Moved swaggerDoc to his own config file

--- src/routes/docs.router.ts
+++ src/routes/docs.router.ts
@@ -1,40 +1,8 @@
 import { Router } from "express";
-import swaggerJsDoc from "swagger-jsdoc";
 import swaggerUI from "swagger-ui-express";
-import { PORT } from "../configs/app.config";
+import { swaggerDoc } from "../configs/swagger.config";
 
-const swaggerDoc: swaggerUI.JsonObject = swaggerJsDoc({
-  swaggerDefinition: {
-    openapi: "3.0.0",
-    info: {
-      title: "CRUD REST API",
-      version: "0.0.1",
-      description:
-        "CRUD REST API that performs the common tasks that the average enterprise-grade or start-up might need",
-      license: {
-        name: "Licensed Under MIT",
-        url: "https://spdx.org/licenses/MIT.html",
-      },
-      contact: {
-        name: "Author's Website",
-        url: "https://algife.com",
-      },
-      servers: [
-        {
-          url: "http://localhost:" + PORT,
-          description: "Development server",
-        },
-      ],
-    },
-  },
-  apis: [
-    // Output Docs with JSDoc comments to auto-generate the API Documentation
-    "src/app.ts",
-    "src/routes/**/**.ts",
-  ],
-});
-
-export const docsRouter: Router = Router();
+const docsRouter: Router = Router();
 
 docsRouter.use(
   "/",
